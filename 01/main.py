2e4291431e2b178ca80ead224ddfa68569169c8a
839560049tan <839560049@qq.com>
2015-04-18 18:10:55 +0800

    CLASS WORK

---
+++ 01/main.py
@@ -0,0 +1,9 @@
+for j in range(9, 0,-1) :
+    for i in range(9,0, -1) :
+        print (i, 'x', j, '=', j*i)
+        # print '%d x %d = %d\t' %(j, i, j*i),
+    print ('\n')
+
+
+print ('\nEND!')
+input()
